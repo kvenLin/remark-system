10d524bb2dfa216ee6a56429d089573f518e66c5
1256233771 <1256233771@qq.com>
2018-11-03 10:53:27 +0800

    last code file

--- src/test/java/com/uchain/remarksystem/dao/ProjectDaoTest.java
+++ src/test/java/com/uchain/remarksystem/dao/ProjectDaoTest.java
@@ -1,25 +1,15 @@
 package com.uchain.remarksystem.dao;
 
-import com.uchain.remarksystem.model.Package;
-import com.uchain.remarksystem.model.Project;
-import com.uchain.remarksystem.service.PackageService;
-import com.uchain.remarksystem.service.ProjectService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Date;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjectDaoTest {
 
-    @Autowired
-    private ProjectService projectService;
-    @Autowired
-    private PackageService packageService;
     @Test
     public void createMock(){
 //        for (int i = 0; i < 30; i++) {
